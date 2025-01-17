c9734bc06736390869845e9d3f5f6980d8951895
Kenneth H. Alvarado <alvaradoken29@gmail.com>
2025-01-17 16:58:58 +0800

    initial profile

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="bg-white grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <Image
           src="/img/Me.jpg"
@@ -10,10 +10,14 @@ export default function Home() {
           width={180}
           height={38}
           priority
+          className="mx-auto  w-fit"
         />
-          <h3>
-            Kenneth H. Alvarado 
+          <h3 className="text-lg text-center w-full text-black">
+            Kenneth H. Alvarado
           </h3>
+          <h1 className="text-black text-center w-full">
+            Bacheclor of Science in Information Technology
+          </h1>
 
 
       </main>
