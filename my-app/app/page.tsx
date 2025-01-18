7acf14d1068336fc6053d2738960dd9c0ab8c6e4
Kenneth H. Alvarado <alvaradoken29@gmail.com>
2025-01-18 16:07:55 +0800

    python

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -2,24 +2,22 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="bg-white grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+    <div className="min-h-screen bg-white p-8">
+      <main className="flex flex-col items-center gap-8">
         <Image
           src="/img/Me.jpg"
-          alt="Next.js logo"
+          alt="Profile picture" 
           width={180}
           height={38}
           priority
-          className="mx-auto  w-fit"
+          className="w-fit"
         />
-          <h3 className="text-lg text-center w-full text-black">
-            Kenneth H. Alvarado
-          </h3>
-          <h1 className="text-black text-center w-full">
-            Bacheclor of Science in Information Technology
-          </h1>
-
-
+        <h3 className="text-lg text-black">
+          Kenneth H. Alvarado
+        </h3>
+        <h1 className="text-black">
+          BACHELOR OF SCIENCE IN INFORMATION TECHNOLOGY
+        </h1>
       </main>
     </div>
   );
