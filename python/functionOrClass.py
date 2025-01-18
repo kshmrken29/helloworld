49690db1fd8b33c8d2d780f86c68beca73728a5e
Kenneth H. Alvarado <alvaradoken29@gmail.com>
2025-01-18 16:29:15 +0800

    python

---
+++ python/functionOrClass.py
@@ -0,0 +1,6 @@
+def my_function(firstName):
+    print(firstName + " is first name")
+
+my_function("Kenneth")
+my_function("Diomari")
+my_function("Kyla")
---
+++ python/variables.py
@@ -0,0 +1,7 @@
+print("Basic Variables Python")
+
+firstName = "Kenneth "
+middleInitial = "H. "
+lastName = "Alvarado"
+
+print(firstName + middleInitial+ lastName)
